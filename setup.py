95b182477e7399e33bf5610b0822fffaee49be91
Graham Preston <graham.preston@gmail.com>
2017-03-08 12:06:00 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from distutils.core import setup
 
 
 setup(name="PyCube",
+      author="Graham Preston",
+      author_email="graham.preston@gmail.com",
+      description="Rubik's Cube timer made with Python",
       url="https://github.com/MegaAmoonguss/pycube",
       install_requires=["js2py", "pillow"])
