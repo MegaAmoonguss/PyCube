d659246420249bec672213d777745bf7fbf65c11
Graham Preston <graham.preston@gmail.com>
2017-03-07 23:03:36 -0500

    Remove button, add space key binding

--- pycube/main.py
+++ pycube/main.py
@@ -1,5 +1,5 @@
 import time
-from tkinter import Tk, Frame, Button, Label
+from tkinter import Tk, Frame, Label
 import os
 import js2py
 
@@ -22,30 +22,25 @@ class PyCube:
 
         self.time_label = Label(frame, text="0.000")
         self.time_label.pack()
-
-        self.time_button = Button(frame,
-                             text="Start",
-                             command=self.start_timer)
-        self.time_button.pack()
+        
+        self.root.bind("<space>", self.timer)
+        self._job = None
+        
         self.root.mainloop()
         
-    def start_timer(self):
-        self.t0 = float("%.3f" % time.time())
-        self.time_button.configure(text="Stop",
-                                   command=self.stop_timer)
-        self.update_timer()
+    def timer(self, event=None):
+        if self._job == None:
+            self.t0 = float("%.3f" % time.time())
+            self.update_timer()
+        else:
+            self.root.after_cancel(self._job)
+            self._job = None
+            scrambler.scramble()
+            self.scramble.configure(text=scrambler.scramblestring(0))
         
     def update_timer(self):
         now = float("%.3f" % (time.time() - self.t0))
         self.time_label.configure(text=now)
         self._job = self.root.after(10, self.update_timer)
-        
-    def stop_timer(self):
-        self.time_button.configure(text="Start",
-                                   command=self.start_timer)
-        self.root.after_cancel(self._job)
-        self._job = None
-        scrambler.scramble()
-        self.scramble.configure(text=scrambler.scramblestring(0))
 
 app = PyCube()
