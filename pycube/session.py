6d88876d595f4f66c61ab6974d0d13da74f58df3
Graham Preston <graham.preston@gmail.com>
2017-03-08 14:30:13 -0500

    Save button working

--- pycube/session.py
+++ pycube/session.py
@@ -12,11 +12,16 @@ class Session:
     def removetime(self, index):
         del self.times[index]
         
+    def gettimes(self):
+        s = ""
+        for t in self.times:
+            s += str(t) + '\n'
+        return s
+        
     def save(self):
         if not os.path.isdir("data"):
             os.makedirs("data")
         
         name = str(datetime.datetime.now())[:-7].replace('-', '').replace(':', '').replace(' ', '')
-        with open(os.path.join("data", name + ".txt"), 'w') as file:
-            for t in self.times:
-                file.write(str(t) + '\n')
+        with open(os.path.join("../data", name + ".txt"), 'w') as file:
+            file.write(self.gettimes())
