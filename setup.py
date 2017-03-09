2a412b7eb2200ef42df040c6b7732e02b283ef54
Graham Preston <graham.preston@gmail.com>
2017-03-09 14:17:23 -0500

    Add version that probably won't be updated for a while but w/e

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 
 setup(name="PyCube",
+      version="0.0.1 alpha",
       author="Graham Preston",
       author_email="graham.preston@gmail.com",
       description="Rubik's Cube timer made with Python",
