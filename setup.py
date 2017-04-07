c6c34ce3f41337762607ae47ed36c000099010f9
Graham Preston <graham.preston@gmail.com>
2017-04-07 13:59:08 -0400

    Add bs4

--- setup.py
+++ setup.py
@@ -6,4 +6,4 @@ setup(name="PyCube",
       author_email="graham.preston@gmail.com",
       description="Rubik's Cube timer made with Python",
       url="https://github.com/MegaAmoonguss/pycube",
-      install_requires=["js2py", "pillow"])
+      install_requires=["js2py", "pillow", "bs4"])
